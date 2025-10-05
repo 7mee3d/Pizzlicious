913242f2c560589195d2dc2d98eb84a3b85a3bf5
Ahmed Jehad <alanturing404@gmail.com>
2025-10-05 13:39:55 +0300

    Edit Assembly Information Title Program to Pizzlicious

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mini Project - Pizza Resto Shodwe")]
+[assembly: AssemblyTitle("Pizzlicious")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
