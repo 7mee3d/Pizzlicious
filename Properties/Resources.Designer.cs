3f066e28187dc5bb10b7d20371729732287cc786
Ahmed Jehad <alanturing404@gmail.com>
2025-10-05 13:38:17 +0300

    Edit the Icon Program Pizzlicious
    Edit the Assembly Name project to Pizzlicious

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Mini_Project___Pizza_Resto_Shodwe.Properties {
+namespace Pizzlicious.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Mini_Project___Pizza_Resto_Shodwe.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mini_Project___Pizza_Resto_Shodwe.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pizzlicious.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
